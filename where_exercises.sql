5e6b4e2f436fe300eed54114c0006ecfa52428d6
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-05-27 14:37:34 -0500

    finished last half of exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -2,7 +2,11 @@ use employees;
 
 SELECT first_name
     FROM employees
-    WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+    WHERE gender = 'M'
+    AND (
+    first_name = 'Irena'
+    OR first_name = 'Vidya'
+    OR first_name = 'Maya');
 
 SELECT *
     FROM employees
@@ -10,12 +14,27 @@ SELECT *
 
 SELECT *
     FROM employees
-    WHERE hire_date BETWEEN 1990-01-01 AND 1999-12-30;
+    WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 
 SELECT *
     FROM employees
-    WHERE birth_date BETWEEN 1900-12-25 AND 2020-12-25;
+    WHERE birth_date LIKE '%12-25%';
 
 SELECT *
     FROM employees
     WHERE last_name LIKE '%q%';
+
+SELECT *
+    FROM employees
+    WHERE last_name LIKE 'E%'
+    OR last_name LIKE '%e';
+
+SELECT *
+    FROM employees
+    WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+    AND birth_date LIKE '%12-25%';
+
+SELECT *
+    FROM employees
+    WHERE last_name LIKE '%q%'
+    AND last_name NOT LIKE '%qu%';
