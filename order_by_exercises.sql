cc91fcf946f748fe321d59b21cde8d1437307e8a
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-05-28 09:25:26 -0500

    finished walk through

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -36,4 +36,4 @@ SELECT *
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
   AND birth_date LIKE '%12-25%'
-    ORDER BY birth_date ASC, hire_date DESC;
+    ORDER BY birth_date, hire_date DESC;
