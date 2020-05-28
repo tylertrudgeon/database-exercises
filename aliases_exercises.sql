3a4c625e55df3370a06de86d41c7822805545017
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-05-28 14:34:08 -0500

    all done on aliases exercise

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,4 +1,5 @@
 use employees;
 
-SELECT CONCAT_WS(' ', first_name, last_name) AS 'full_name', birth_date AS 'DOB'
+SELECT CONCAT(emp_no, ' - ', first_name, ' ', last_name) AS 'full_name', birth_date AS 'DOB'
 FROM employees;
+
