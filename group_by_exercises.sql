3a264e0d00df05e37ccbcc9ca7dec0bb8f915778
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-05-28 14:13:55 -0500

    finished 3, 4, and 5

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,5 +1,28 @@
-use titles;
+USE titles;
+USE employees;
 
-select distinct title
-from titles;
+SELECT DISTINCT title
+FROM titles;
 
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+   AND last_name LIKE '%e';
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE '%e'
+GROUP BY first_name, last_name;
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+AND NOT last_name LIKE '%qu%'
+GROUP BY last_name;
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND NOT last_name LIKE '%qu%'
+GROUP BY last_name;
