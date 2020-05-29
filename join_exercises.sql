d44226ec75ed0dd18c672fcd1f1f99388cbac233
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-05-29 10:43:29 -0500

    finished first question of employees database exercise

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,20 +1,30 @@
-use join_test_db;
+# use join_test_db;
+#
+# select *
+# FROM users as u
+# JOIN roles as r ON u.role_id = r.id;
+#
+# select *
+# FROM users AS u
+# LEFT JOIN roles AS r ON u.role_id = r.id;
+#
+# select *
+# FROM users AS u
+# RIGHT JOIN roles AS r on u.role_id = r.id;
+#
+# SELECT COUNT(*) AS user_roles
+# FROM users AS u
+# RIGHT JOIN roles AS r on u.role_id = r.id
+# GROUP BY r.id
+# ORDER BY r.id;
 
-select *
-FROM users as u
-JOIN roles as r ON u.role_id = r.id;
+USE employees;
 
-select *
-FROM users AS u
-LEFT JOIN roles AS r ON u.role_id = r.id;
-
-select *
-FROM users AS u
-RIGHT JOIN roles AS r on u.role_id = r.id;
-
-SELECT COUNT(*) AS user_roles
-FROM users AS u
-RIGHT JOIN roles AS r on u.role_id = r.id
-GROUP BY r.id
-ORDER BY r.id;
-
+SELECT CONCAT(dept_name) AS Department_Name, CONCAT_WS(' ',first_name, last_name) AS Department_Manager
+FROM employees AS e
+JOIN dept_manager AS dm
+    ON e.emp_no = dm.emp_no
+JOIN departments AS d
+    ON dm.dept_no = d.dept_no
+WHERE to_date = '9999-01-01'
+ORDER BY dept_name;
