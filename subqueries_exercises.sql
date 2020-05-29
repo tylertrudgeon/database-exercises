f9cd4a404177823bfb87832046631ed4395c5204
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-05-29 14:55:16 -0500

    finished second and third part of subqueries exercise

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -8,3 +8,23 @@ WHERE hire_date IN (
     WHERE emp_no = 101010
     );
 
+SELECT *
+FROM employees e
+JOIN titles t
+    on e.emp_no = t.emp_no
+WHERE first_name IN (
+    SELECT first_name
+    FROM employees
+    WHERE first_name = 'Aamod'
+    );
+
+SELECT first_name, last_name
+FROM employees
+JOIN dept_manager dm
+    on employees.emp_no = dm.emp_no
+WHERE gender IN (
+    SELECT gender
+    FROM dept_manager
+    WHERE gender = 'F'
+    AND dm.to_date > curdate()
+    )
