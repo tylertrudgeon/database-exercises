91ada6f3ccdd5caf0da2e674cc5cd314ec96a1e8
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-05-29 11:09:30 -0500

    finished third question of employees database exercise

--- join_exercises.sql
+++ join_exercises.sql
@@ -31,11 +31,23 @@ ORDER BY dept_name;
 
 SELECT CONCAT(dept_name) AS Department_Name, CONCAT_WS(' ',first_name, last_name) AS Department_Manager
 FROM employees AS e
-         JOIN dept_manager AS dm
-              ON e.emp_no = dm.emp_no
-         JOIN departments AS d
-              ON dm.dept_no = d.dept_no
+JOIN dept_manager AS dm
+    ON e.emp_no = dm.emp_no
+JOIN departments AS d
+    ON dm.dept_no = d.dept_no
 WHERE to_date = '9999-01-01'
     AND e.gender = 'F'
 ORDER BY dept_name;
 
+SELECT title AS Title, COUNT(*) AS Count
+FROM employees AS e
+JOIN dept_emp de
+    ON e.emp_no = de.emp_no
+JOIN departments d
+    ON de.dept_no = d.dept_no
+JOIN titles t
+    ON e.emp_no = t.emp_no
+WHERE dept_name = 'Customer Service'
+GROUP BY title;
+
+describe dept_emp;
